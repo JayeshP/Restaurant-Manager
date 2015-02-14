9931529840d998edb9ce4db36c56cb6b60de7f1d
Jayesh Pawar <jayeshrpawar@gmail.com>
2015-02-14 14:30:42 +0530

    badis setup of symfony done

---
+++ src/RestMan/ApiBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RestMan\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('RestManApiBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
